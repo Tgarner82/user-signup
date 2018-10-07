0c872264db403c9eb75f36c342f8867422011d44
Tyrone Garner II <tyroneegarner@gmail.com>
2018-10-07 09:51:04 -0500

    User-Signup revamp

--- main.py
+++ main.py
@@ -1,66 +1,81 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import cgi
 import os
-import jinja2
 import re
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True )
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    template = jinja_env.get_template('signup_form.html')
-    return template.render()
+    return render_template('signup_form.html')
 
+@app.route("/")
+def display():
+    return render_template('signup_form.html', user_name=user_name, user_password=user_password,
+    user_verify=user_verify, user_email=user_email, user_error='', password_error='', 
+    verify_error='', email_error='')
 
 @app.route("/", methods=['POST'])
 def welcome():
 
     user_name = request.form['user_name']
     user_password = request.form['user_password']
-    user_verify = request.form['user_verify_password']
+    user_verify = request.form['user_verify']
     user_email = request.form['user_email']
-    error = ''
-    
-    
 
-
-    if  user_name == '' or user_password == '' or user_verify == '':
-        error = 'Please do not leave an empty field.'
-        user_name = ''
-        template = jinja_env.get_template('signup_form.html')
-        return template.render(error=error)
-    elif re.search(r'\s',user_name) or re.search(r'\s',user_password):
-        error = 'Invalid. Please do not use spaces.'
-        template = jinja_env.get_template('signup_form.html')
-        return template.render(error=error)
-    elif len(user_name) < 3 or len(user_name) > 20:
-        error = 'User name must be greater than 3 or less than 20 characters.'
-        template = jinja_env.get_template('signup_form.html')
-        return template.render(error=error)
-    elif len(user_password) < 3 or len(user_password) > 20:
-        error = 'Password must be greater than 3 or less than 20 characters.'
-        template = jinja_env.get_template('signup_form.html')
-        return template.render(error=error)
-    elif not user_password == user_verify:
-        error = 'Password must match to verify.'
-        template = jinja_env.get_template('signup_form.html')
-        return template.render(error=error)
-    elif len(user_email) < 3 or len(user_email) > 20:
-        error = 'Invalid Email.'
-        template = jinja_env.get_template('signup_form.html')
-        return template.render(error=error)
-    elif not re.match(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$',user_email):
-        error = 'Invalid Email.'
-        template = jinja_env.get_template('signup_form.html')
-        return template.render(error=error)
+    user_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+    
+    if user_name == '':
+        user_error = 'Please do not leave an empty field.'
     else:
-        template = jinja_env.get_template('welcome.html')
-        return template.render(name=user_name) 
-    
+        user_name = user_name 
+        if len(user_name) < 3 or len(user_name) > 20:
+            user_error = 'User name must be longer than 3 and shorter than 20 characters.'
+        else:
+            user_name=user_name
+            if re.search(r'\s',user_name):
+                user_error = 'Please do not use a space.'
+            else:
+                user_name=user_name
+    if user_password == '':
+        password_error = 'Please do not leave an empty field.'
+    else:
+        user_password=user_password
+        if len(user_password) < 3 or len(user_password) > 20:
+            password_error = 'User name must be longer than 3 and shorter than 20 characters.'
+        else:
+            user_password=user_password
+            if re.search(r'\s', user_password):
+                password_error = 'Please do not use a space'
+            else:
+                user_password=user_password
+    if user_verify == '':
+        verify_error = 'Please do not leave an empty field.'
+    else:
+        user_verify=user_verify
+        if user_verify != user_password:
+            verify_error = 'Password must match.'
+        else:
+            user_verify=user_verify
+    if not re.match(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$',user_email):
+        email_error = 'Invalid Email.'
+    else:
+        user_email=user_email
+        if len(user_email) < 3 or len(user_email) > 20:
+            email_error = 'Email must be longer than 3 and shorter than 20 characters.'
+        else:
+            user_email=user_email
+    if not user_error and not password_error and not verify_error and not email_error:
+        return render_template('welcome.html', name=user_name)
+    else:
+        return render_template('signup_form.html', user_error=user_error, password_error=password_error, 
+        verify_error=verify_error, email_error=email_error)
 
-              
+    
+             
 app.run()
